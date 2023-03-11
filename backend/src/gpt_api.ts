597278c2ba1d7b3af09df81e9632f6f496813670
Matěj Kripner <kripnermatej@gmail.com>
2023-03-11 15:20:09 +0100

    Use axios.

--- backend/src/gpt_api.ts
+++ backend/src/gpt_api.ts
@@ -1,39 +1,49 @@
-// Write a ChatGPT API Wrapper
+import keys from "./secret-config"
+import axios from "axios";
 
-// const API_URL = 'https://api.chatgpt.com/v1/';
-
-// export default class ChatGPT {
-//     private apiKey: string;
-    
-//     constructor(apiKey: string) {
-//         this.apiKey = apiKey;
+// async function foo() {
+//     const configuration = new Configuration({
+//         organization: "org-10yq50qLRI3ntaeTQqPsIH7pE",
+//         apiKey: config.OPENAPI_KEY,
+//     });
+//     const openai = new OpenAIApi(configuration);
+//     try {
+//         const response = await openai.listModels();
+//         console.log(response);
+//     } catch (error) {
+//         if (error.response) {
+//             console.log(error.response.status);
+//             console.log(error.response.data);
+//         } else {
+//             console.log(error.message);
+//         }
 //     }
-    
-//     async sendMessage(message: string): Promise<string> {
-//         const { data } = await fetch(API_URL, {
-//         message,
-//         apiKey: this.apiKey,
-//         });
-    
-//         return data.message;
-//     }
-//     }
+// }
 
-// // Path: app/src/index.ts
-
-// import ChatGPT from './gpt_api'; 
-
-import { Configuration, OpenAIApi } from "openai";
-import config from "./secret-config"
-
-async function foo() {
-    const configuration = new Configuration({
-        organization: "org-10yq50qLRI3ntaeTQqPsIH7pE",
-        apiKey: config.OPENAPI_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-    const response = await openai.listModels();
-    console.log(response);
+async function GetGPTResponse() {
+    let config = {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + keys.OPENAPI_KEY,
+        },
+        data: {
+            "model": "gpt-3.5-turbo",
+            "messages": [{ "role": "user", "content": "Say this is a test!" }],
+            "temperature": 0.7
+        }
+    }
+    // create HTTP request using axios
+    try {
+        const response = await axios.post("https://api.openai.com/v1/chat/completions", config)
+        return response
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+    }
 }
 
-export default {foo}
+export default { GetGPTResponse }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,9 @@ import gpt from "./gpt_api"
 const app: Application = express()
 const port: number = 3001
 app.get("/", (req: Request, res: Response) => {
- res.send("Hello toto")
- gpt.foo();
+    let result = gpt.GetGPTResponse();
+    res.send(result)
 })
 app.listen(port, function () {
- console.log(`App is listening on port ${port} !`)
+    console.log(`App is listening on port ${port} !`)
 })
